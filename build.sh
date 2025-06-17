e8a44cdfa93c77e8d3748981963c574a0f654bfd
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2025-06-17 17:53:31 +0300

    Swith to using iossifovlab-impala4 from docker hub

--- build.sh
+++ build.sh
@@ -143,7 +143,7 @@ EOT
   {
     local -A ctx_impala
     build_run_ctx_init ctx:ctx_impala "persistent" "container" \
-        "iossifovlab/seqpipe-impala4:latest" \
+        "iossifovlab/iossifovlab-impala4:latest" \
         "cmd-from-image" "no-def-mounts" \
         ports:21050,8020 --hostname impala --network "${ctx_network["network_id"]}"
 
