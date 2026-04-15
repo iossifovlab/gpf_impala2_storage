dda766ccbb4c1092ac33908b9d6e51a4d4f35b23
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2026-04-15 15:47:01 +0300

    Fixes after package distribution package reorganization

--- build.sh
+++ build.sh
@@ -124,8 +124,8 @@ EOT
     local gpf_dev_tag
     gpf_dev_tag="$(e docker_img_gpf_dev_tag)"
     build_docker_image_create "$gpf_impala2_storage_image" \
-        "projects/iossifovlab.gpf.repo/impala2_storage" \
-        "projects/iossifovlab.gpf.repo/impala2_storage/Dockerfile" \
+        "projects/iossifovlab.gpf.repo/gpf_impala2_storage" \
+        "projects/iossifovlab.gpf.repo/gpf_impala2_storage/Dockerfile" \
         "$gpf_dev_tag"
     gpf_impala2_storage_image_ref="$(e docker_img_gpf_impala2_storage_dev)"
   }
@@ -170,14 +170,14 @@ EOT
 
     build_run_container scripts/wait-for-it.sh -h impala -p 21050 -t 300
 
-    for d in $project_dir/gain_core $project_dir/gpf_core $project_dir/gpf_web $project_dir/impala2_storage; do
+    for d in $project_dir/gain_core $project_dir/gpf_core $project_dir/gpf_web $project_dir/gpf_impala2_storage; do
       build_run_container bash -c 'cd "'"${d}"'"; /opt/conda/bin/conda run --no-capture-output -n gpf \
         pip install -e .'
     done
 
     build_run_container bash -c '
         project_dir="/wd/projects/iossifovlab.gpf.repo";
-        cd $project_dir/impala2_storage;
+        cd $project_dir/gpf_impala2_storage;
         export PYTHONHASHSEED=0;
         /opt/conda/bin/conda run --no-capture-output -n gpf py.test -v \
           --durations 20 \
@@ -190,7 +190,7 @@ EOT
 
     build_run_container bash -c '
         project_dir="/wd/projects/iossifovlab.gpf.repo";
-        cd $project_dir/impala2_storage;
+        cd $project_dir/gpf_impala2_storage;
         export PYTHONHASHSEED=0;
         /opt/conda/bin/conda run --no-capture-output -n gpf py.test -v \
           --durations 20 \
@@ -198,13 +198,13 @@ EOT
           --junitxml=/wd/results/impala2-storage-integration-junit.xml \
           --cov-append --cov impala2_storage \
           $project_dir/gpf_core/tests/integration/genotype_storage \
-          --gsf $project_dir/impala2_storage/impala2_storage/tests/impala2_storage.yaml || true'
+          --gsf $project_dir/gpf_impala2_storage/impala2_storage/tests/impala2_storage.yaml || true'
 
     build_run_container cp /wd/results/impala2-storage-integration-junit.xml /wd/test-results/
 
     build_run_container bash -c '
         project_dir="/wd/projects/iossifovlab.gpf.repo";
-        cd $project_dir/impala2_storage;
+        cd $project_dir/gpf_impala2_storage;
         if [ -f ".coverage" ]; then
             coverage xml;
             cp -f coverage.xml /wd/test-results/;
